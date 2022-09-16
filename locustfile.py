df14173a1eea93d6ef6dfc8984058e4a589878c9
dawitanelay <57917956+dawitanelay@users.noreply.github.com>
2022-09-16 15:48:07 +0200

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,32 +1,32 @@
-import time
-from locust import HttpUser, task
+from locust import HttpUser, between, task
 
-class QuickstartUser(HttpUser):
+class WebsiteUser(HttpUser):
+    host = "https://flskml-app.azurewebsites.net/:443"
+    wait_time = between(5, 15)
+
     @task
     def index(self):
-        self.client.get('/')
+        self.client.get("/")
 
     @task
-    def post_predict_house_price(self):
-        self.client.post("/predict",json={
-        "CHAS":{
-            "0":0
-        },
-        "RM":{
-            "0":6.575
-        },
-        "TAX":{
-            "0":296.0
-        },
-        "PTRATIO":{
-            "0":15.3
-        },
-        "B":{
-            "0":396.9
-        },
-        "LSTAT":{
-            "0":4.98
-        }
+    def predict(self):
+        self.client.post("/predict", json={
+            "CHAS":{
+                "0":0
+            },
+            "RM":{
+                "0":6.575
+            },
+            "TAX":{
+                "0":296.0
+            },
+            "PTRATIO":{
+                "0":15.3
+            },
+            "B":{
+                "0":396.9
+            },
+            "LSTAT":{
+                "0":4.98
+            }
         })
-
-   
