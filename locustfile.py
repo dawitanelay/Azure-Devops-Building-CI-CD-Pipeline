28dd59e6b8b17955fbe79920378a1cedd340ea93
dawitanelay <dawitanelay@gmail.com>
2022-09-19 17:22:01 +0200

    fix json format on loucst

--- locustfile.py
+++ locustfile.py
@@ -1,55 +1,55 @@
-from locust import HttpUser, between, task
+from locust import HttpUser, task, between
 
-class WebsiteUser(HttpUser):
-    host = "https://flskml-app.azurewebsites.net/:443"
-    wait_time = between(5, 15)
+class WebsiteTestUser(HttpUser):
+    wait_time = between(0.5, 3.0)
 
-    @task
-    def index(self):
-        self.client.get("/")
+    @task(1)
+    def test_root(self):
+        self.client.get("")
 
     @task
     def predict(self):
         self.client.post("/predict", json={
             {
-   "CRIM": {
-      "0":0.21124
-   },
-   "ZN":{
-      "0":12.5
-   },
-   "INDUS":{
-      "0":7.87
-   },
-   "CHAS":{
-      "0":0
-   },
-   "NOX":{
-      "0":0.524
-   },
-   "RM":{
-      "0":5.631
-   },
-   "AGE":{
-      "0":100.0
-   },
-   "DIS":{
-      "0":6.0821
-   },
-   "RAD":{
-      "0":5.0
-   },
-   "TAX":{
-      "0":311.0
-   },
-   "PTRATIO":{
-      "0":15.2
-   },
-   "B":{
-      "0":386.63
-   },
-   "LSTAT":{
-      "0":29.93
-   }
-}
+
+     "CRIM": {
+        "0":0.21124
+     },
+     "ZN":{
+        "0":12.5
+     },
+     "INDUS":{
+        "0":7.87
+     },
+     "CHAS":{
+        "0":0
+     },
+     "NOX":{
+        "0":0.524
+     },
+     "RM":{
+        "0":5.631
+     },
+     "AGE":{
+        "0":100.0
+     },
+     "DIS":{
+        "0":6.0821
+     },
+     "RAD":{
+        "0":5.0
+     },
+     "TAX":{
+        "0":311.0
+     },
+     "PTRATIO":{
+        "0":15.2
+     },
+     "B":{
+        "0":386.63
+     },
+     "LSTAT":{
+        "0":29.93
+     }
+  }
         })
--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -46,5 +46,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flaskml-app.azurewebsites.net:$PORT/predict
+     -X POST https://flask-ml-app.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
